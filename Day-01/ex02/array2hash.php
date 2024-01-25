1060997ee893df218a4b8f9dc203f2fe7e9a70d8
Adnen Aloui <adnen.aloui2@hotmail.com>
2024-01-25 17:02:46 +0100

    PHP-01 MAJ. Il sera presque READY

--- Day-01/ex02/array2hash.php
+++ Day-01/ex02/array2hash.php
@@ -1,13 +1,14 @@
 <?php
 function array2hash($array)
 {
-    $hash = array();
+    $hash = array(); // Il s'agit d'un tableau associatif, chaque clé (lâge) doit être unique, sinon, la valeur sera remplacée par l'itération qui aura le même âge
+    //exemple : Pierre, 30 et Mary, 30, le tableau ne contiendra que Mary, 30 a lieu de Pierre et Mary
     $i = -1;
     while (++$i < count($array))
     {
         if (count($array[$i]) == 2)
-            $hash[$array[$i][1]] = $array[$i][0];
-    }
+            $hash[$array[$i][1]] = $array[$i][0]; // Assignation du nom à l'âge. $array[$i][0] est le name et $array[$i][1] est l'âge. On met le tout dans un tableau.
+    } // age = clé et name = valeur
     return $hash;
 }
 ?>
--- Day-01/ex03/array2hash_sorted.php
+++ Day-01/ex03/array2hash_sorted.php
@@ -3,13 +3,14 @@ function array2hash_sorted($array)
 {
     $hash = array();
     $i = -1;
-	usort($array, function($a, $b) {
-		return strcmp($b[0], $a[0]);
-	});    
+	usort($array, function($a, $b) // usort ne trie pas en lui même les arguments, c'est nous, via la fonction anonyme qui allons le lui indiquer.
+    {
+		return strcmp($b[0], $a[0]); // Si on nombre négatif est renvoyé en callback, alors usort va inverser les tableaux 
+	}); // [0] correspond aux noms et [1] à l'age, c'est pour celaa qu'on met [0]   
 	while (++$i < count($array))
     {
         if (count($array[$i]) == 2)
-            $hash[$array[$i][0]] = $array[$i][1];
+            $hash[$array[$i][0]] = $array[$i][1]; // On inverse l'assignation, c'est l'âge qui est assigné au nom (nom = clé, age = valeur)
     }
     return $hash;
 }
