781e5d583cab69829d0af541799c281a9bc5736a
Winspear22 <adnen.aloui2@hotmail.com>
2024-02-21 14:01:56 +0100

    Correction de l'ex00 PHP-00 qui passe désormais la correction

--- Day-00/ex00/myawesomescript.sh
+++ Day-00/ex00/myawesomescript.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/usr/bin/sh
 
 url="$1"
 
-if [[ ! $url == https://* ]]; then
-    url="https://$1"
+if echo "$url" | grep -q "^https://"; then
+    final_url="$url"
+    curl -sI "$final_url" | grep "location" | cut -d ' ' -f 2
+else
+    final_url="$url"
+    curl -sI "$final_url" | grep "Location" | cut -d ' ' -f 2
 fi
-
-curl -sI $url | grep "location" | cut -d ' ' -f 2
