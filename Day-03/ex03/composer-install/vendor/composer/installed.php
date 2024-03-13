bb17106476626fb7c470c8ccaa5844c2fe4fd379
Winspear22 <adnen.aloui2@hotmail.com>
2024-03-13 14:57:56 +0100

    PHP03 -- ex03 : essaie à nouveau

---
+++ Day-03/ex03/composer-install/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/adaloui',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a3e9ecc52556eba14bc796773de4d571a32a3d3d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/adaloui' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a3e9ecc52556eba14bc796773de4d571a32a3d3d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
