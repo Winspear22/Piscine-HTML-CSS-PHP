aa1b2f35e3ccd82bc1568a49b2cb644e415d376f
Winspear22 <adnen.aloui2@hotmail.com>
2024-03-13 14:11:07 +0100

    PHP03 -- ex01 : Amélioration des scripts où on peut require et install indépendament

--- Day-03/ex01/script-require.sh
+++ Day-03/ex01/script-require.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Définition des dossiers et des commandes require correspondantes
+# Définition des dossiers et des commandes require correspondantes avec --no-update
 declare -A commands=(
-    ["monolog-2.0.0-2.0.2"]='composer require monolog/monolog:"^2.0.0 <2.0.2"'
-    ["monolog-2.0.0-2.3.5"]='composer require monolog/monolog:"^2.2.1 <=2.3.5"'
-    ["monolog-2.1.0-2.2.0"]='composer require monolog/monolog:">=2.1.0 <=2.2.0"'
-    ["monolog-2.2.0-2.3.5"]='composer require monolog/monolog:">2.0.0 <2.3.5"'
-    ["monolog-2.3.0"]='composer require monolog/monolog:"^2.3.1"'
+    ["monolog-2.0.0-2.0.2"]='composer require monolog/monolog:"^2.0.0 <2.0.2" --no-update'
+    ["monolog-2.0.0-2.3.5"]='composer require monolog/monolog:"^2.2.1 <=2.3.5" --no-update'
+    ["monolog-2.1.0-2.2.0"]='composer require monolog/monolog:">=2.1.0 <=2.2.0" --no-update'
+    ["monolog-2.2.0-2.3.5"]='composer require monolog/monolog:">2.0.0 <2.3.5" --no-update'
+    ["monolog-2.3.0"]='composer require monolog/monolog:"^2.3.1" --no-update'
 )
 
 # Boucle sur chaque dossier et exécution de la commande require correspondante
 for dir in "${!commands[@]}"; do
-    echo "Installation de Monolog dans le dossier $dir..."
+    echo "Ajout de Monolog au fichier composer.json dans le dossier $dir sans installer..."
     (cd "$dir" && eval ${commands[$dir]} && cd - > /dev/null)
 done
