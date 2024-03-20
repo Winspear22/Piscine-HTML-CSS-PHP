d005e97969cdc67e126e429b9a9733843a98b1f0
Winspear22 <adnen.aloui2@hotmail.com>
2024-03-20 10:16:05 +0100

    PHP03 -- TERMINE

--- Day-03/ex01/script-require.sh
+++ Day-03/ex01/script-require.sh
@@ -2,11 +2,11 @@
 
 # Définition des dossiers et des commandes require correspondantes avec --no-update
 declare -A commands=(
-    ["monolog-2.0.0-2.0.2"]='composer require monolog/monolog:"^2.0.0 <2.0.2" --no-update'
-    ["monolog-2.0.0-2.3.5"]='composer require monolog/monolog:"^2.2.1 <=2.3.5" --no-update'
-    ["monolog-2.1.0-2.2.0"]='composer require monolog/monolog:">=2.1.0 <=2.2.0" --no-update'
-    ["monolog-2.2.0-2.3.5"]='composer require monolog/monolog:">2.0.0 <2.3.5" --no-update'
-    ["monolog-2.3.0"]='composer require monolog/monolog:"^2.3.1" --no-update'
+    ["monolog-2.0.0-2.0.2"]='composer require monolog/monolog:"^2.0.0 <2.0.2"'
+    ["monolog-2.0.0-2.3.5"]='composer require monolog/monolog:"^2.2.1 <=2.3.5"'
+    ["monolog-2.1.0-2.2.0"]='composer require monolog/monolog:">=2.1.0 <=2.2.0"'
+    ["monolog-2.2.0-2.3.5"]='composer require monolog/monolog:">2.0.0 <2.3.5"'
+    ["monolog-2.3.0"]='composer require monolog/monolog:"^2.3.1"'
 )
 
 # Boucle sur chaque dossier et exécution de la commande require correspondante
