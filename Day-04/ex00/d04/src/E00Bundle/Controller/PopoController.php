de02bd9e88b03fa32c8a77fb6c14ed7fb7534610
Winspear22 <adnen.aloui2@hotmail.com>
2024-03-21 14:12:17 +0100

    PHP04 -- ex00 : terminé

--- Day-04/ex00/d04/src/E00Bundle/Controller/PopoController.php
+++ Day-04/ex00/d04/src/E00Bundle/Controller/PopoController.php
@@ -5,20 +5,30 @@ namespace App\E00Bundle\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class PopoController extends AbstractController
 {
     /**
-    * @Route("/annotation", name="annotation")
+    * @Route("/e00/firstpage", name="first_page")
     */  
     public function helloFromAnnotation()
     {
-        return new Response('Hello world! From Annotation');
+        return new Response('Hello world!');
+    }
+
+    /**
+    * @Route("/{url}", requirements={"url"=".+"})
+    */
+    public function index(): Response
+    {
+        return new Response(
+            '<html><body><h1>Error 404 - PHP04 : adaloui</h1></body></html>',
+            Response::HTTP_NOT_FOUND
+        );
     }
 
     
-    #[Route('/attribute', name: 'attribute')]
+    #[Route('/e00/firstpageAttribute', name: 'first_pageAttribute')]
     public function helloFromAttribute()
     {
         return new Response('Hello world! from Attribute');
