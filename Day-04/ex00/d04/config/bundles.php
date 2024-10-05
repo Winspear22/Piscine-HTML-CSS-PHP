c254a8a78ef2573c2e971d61dfa4e27bd471f78d
Winspear22 <adnen.aloui2@hotmail.com>
2024-10-05 13:38:31 +0200

    Deleted folder 'mon_dossier' from retry-42

--- Day-04/ex00/d04/config/bundles.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    App\E00Bundle\PopoBundle::class => ['all' => true],
-];
--- Day-04/ex01/d04/config/bundles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    App\E00Bundle\PopoBundle::class => ['all' => true],
-    App\E01Bundle\LoloBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-];
