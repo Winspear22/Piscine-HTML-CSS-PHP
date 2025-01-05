73dc3de0c0dd2a5f42679ef2c80eace657c4afb6
Winspear22 <adnen.aloui2@hotmail.com>
2025-01-05 22:18:43 +0100

    PHP05 -- ex00 : TERMINE.

--- Day-05/ex00/src/Ex00Bundle/Controller/Ex00Controller.php
+++ Day-05/ex00/src/Ex00Bundle/Controller/Ex00Controller.php
@@ -14,26 +14,53 @@ class Ex00Controller extends AbstractController
 	 */
 	public function createTable(Connection $connection): Response
     {
-        $sql = "CREATE TABLE IF NOT EXISTS users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            username VARCHAR(255) UNIQUE NOT NULL,
-            name VARCHAR(255) NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            enable BOOLEAN NOT NULL,
-            birthdate DATETIME NOT NULL,
-            address LONGTEXT NOT NULL
-        )";
+		$message = null;
+		try
+		{
+			$checkTable = $connection->executeQuery("SHOW TABLES LIKE 'user'")->rowCount();
+			if ($checkTable > 0) 
+                $message = "La table 'user' existe déjà.";
+			else 
+			{
+                // Création de la table si elle n'existe pas
+                $sql = "CREATE TABLE IF NOT EXISTS user (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    username VARCHAR(255) UNIQUE NOT NULL,
+                    name VARCHAR(255) NOT NULL,
+                    email VARCHAR(255) UNIQUE NOT NULL,
+                    enable BOOLEAN NOT NULL,
+                    birthdate DATETIME NOT NULL,
+                    address LONGTEXT NOT NULL
+                )";
+                $connection->executeStatement($sql);
+                $message = "Table 'user' créée avec succès !";
+            }
+        }
+		catch (\Exception $e) 
+		{
+            $message = "Erreur lors de la création de la table : " . $e->getMessage();
+        }
 
+        return $this->render('create_table.html.twig', [
+            'message' => $message,
+        ]);
+    }
+
+	 /**
+     * @Route("/ex00/delete", name="ex00_delete_table")
+     */
+    public function deleteTable(Connection $connection): Response
+    {
         $message = null;
 
         try 
 		{
-            $connection->executeStatement($sql);
-            $message = "Table 'users' créée avec succès !";
+            $connection->executeStatement("DROP TABLE IF EXISTS user");
+            $message = "Table 'user' supprimée avec succès !";
         } 
 		catch (\Exception $e) 
 		{
-            $message = "Erreur lors de la création de la table : " . $e->getMessage();
+            $message = "Erreur lors de la suppression de la table : " . $e->getMessage();
         }
 
         return $this->render('create_table.html.twig', [
