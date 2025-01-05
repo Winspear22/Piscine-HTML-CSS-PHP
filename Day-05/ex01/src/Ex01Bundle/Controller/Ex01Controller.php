9a9e14f002b9ae3342e73649ae393627765e5933
Winspear22 <adnen.aloui2@hotmail.com>
2025-01-05 18:28:14 +0100

    PHP05 -- ex01 : le fichier de départ est fonctionnel, Hello World s'affiche correctement.

--- Day-05/ex01/src/Ex01Bundle/Controller/Ex01Controller.php
+++ Day-05/ex01/src/Ex01Bundle/Controller/Ex01Controller.php
@@ -2,18 +2,20 @@
 
 namespace App\Ex01Bundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\DBAL\Connection;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class Ex01Controller extends AbstractController
 {
 
-	/*
+	/**
 	*	@Route("/ex01", name="ex01_create_table")
 	*/
-	public function createTable()
+	public function createTable(): Response
 	{
-		
+		return new Response('hello world');
 	}
 
 }
