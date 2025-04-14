0c84c2a441be20bca70041978d2d2a165d1f0f04
user42 <user42@user42.user42.org>
2025-04-14 18:19:42 +0200

    PHP05 -- ex00 : le site semble OK, je vais rajouter un bouton pour supprimer et enjoliver un peu le site.

--- Day-05-restart/ex00/var/cache/dev/App_KernelDevDebugContainer.php
+++ Day-05-restart/ex00/var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \Container5YlvWwx\App_KernelDevDebugContainer([
     'container.build_hash' => '5YlvWwx',
-    'container.build_id' => '9a8fc07d',
-    'container.build_time' => 1744645368,
+    'container.build_id' => '68ff331a',
+    'container.build_time' => 1744645889,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'Container5YlvWwx');
