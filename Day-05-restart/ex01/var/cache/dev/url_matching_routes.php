775a29dc8998902d411695119269762048310586
Winspear22 <adnen.aloui2@hotmail.com>
2025-04-20 15:35:26 +0200

    PHP05 -- ex01 : création de l'entité user.

---
+++ Day-05-restart/ex01/var/cache/dev/url_matching_routes.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/ex01' => [[['_route' => 'ex01bundle_index', '_controller' => 'App\\Ex01Bundle\\Controller\\Ex01Controller::index'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        35 => [
+            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
