5e70c383bbc6849c16c18a3ef005bb09cd946f47
user42 <user42@user42.user42.org>
2025-05-05 17:45:54 +0200

    PHP05 -- ex02 : avancement sur INSERT

--- Day-05-restart/ex02/src/Ex02Bundle/Controller/Ex02Controller.php
+++ Day-05-restart/ex02/src/Ex02Bundle/Controller/Ex02Controller.php
@@ -2,9 +2,16 @@
 
 namespace App\Ex02Bundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class Ex02Controller extends AbstractController
 {
@@ -13,15 +20,33 @@ class Ex02Controller extends AbstractController
      */
     public function index(): Response
     {
-        return new Response("Hello from index!");
+        //return new Response("Hello from ex02insert!");
+        return $this->render('index.html.twig');
     }
 
-        /**
+    /**
      * @Route("/ex02/insert", name="ex02insert")
      */
     public function insert(): Response
     {
-        return new Response("Hello from ex02insert!");
+        $message = "";
+        try
+        {
+            $form = $this->createFormBuilder()
+                ->add('username', TextType::class)
+                ->add('name', TextType::class)
+                ->add('email', EmailType::class)
+                ->add('enable', CheckboxType::class, ['required' => false])
+                ->add('birthdate', DateType::class, ['widget' => 'single_text'])
+                ->add('address', TextareaType::class)
+                ->add('submit', SubmitType::class, ['label' => 'Ajouter l’utilisateur'])
+                ->getForm();
+        }
+        catch (\Exception $e)
+        {
+            $message = "Erreur lors de l'usage de la commande INSERT : " . $e->getMessage();
+        }
+        return $this->render('insert.html.twig', ['form' => $form->createView()]);
     }
 
     /**
@@ -29,6 +54,15 @@ class Ex02Controller extends AbstractController
      */
     public function select(): Response
     {
-        return new Response("Hello from ex02select!");
+        $message = "";
+        try
+        {
+
+        }
+        catch (\Exception $e)
+        {
+            $message = "Erreur lors de l'usage de la commande SELECT : " . $e->getMessage();
+        }
+        //return new Response("Hello from ex02select!");
     }
 }
