43dbc62a4e7eb0dec258d0f6e4ad49a747bfae50
user42 <user42@user42.user42.org>
2025-05-05 20:15:33 +0200

    PHP05 -- ex03 : ajout des 4 routes

--- Day-05-restart/ex03/src/Ex03Bundle/Controller/Ex03Controller.php
+++ Day-05-restart/ex03/src/Ex03Bundle/Controller/Ex03Controller.php
@@ -9,10 +9,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class Ex03Controller extends AbstractController
 {
     /**
-     * @Route("/ex03bundle", name="ex03bundle_index")
+     * @Route("/ex03", name="ex03bundle_index")
      */
     public function index(): Response
     {
-        return new Response("Hello from Ex03Controller!");
+        return new Response("Hello from Ex03index!");
+    }
+
+    /**
+     * @Route("/ex03insert", name="ex03bundle_insert")
+     */
+    public function insert(): Response
+    {
+        return new Response("Hello from Ex03insert!");
+    }
+
+    /**
+     * @Route("/ex03select", name="ex03bundle_select")
+     */
+    public function select(): Response
+    {
+        return new Response("Hello from Ex03select!");
+    }
+
+    /**
+     * @Route("/ex03delete", name="ex03bundle_delete")
+     */
+    public function delete(): Response
+    {
+        return new Response("Hello from Ex03delete!");
     }
 }
