d81afbe9954fd1394f3ef6316560bf3e2c200fba
Winspear22 <adnen.aloui2@hotmail.com>
2025-06-30 19:27:38 +0200

    PHP05 -- ex05 : ajout de la fonction delete.

--- Day-05-restart/ex05-retry/src/Ex05Bundle/Controller/Ex05Controller.php
+++ Day-05-restart/ex05-retry/src/Ex05Bundle/Controller/Ex05Controller.php
@@ -48,4 +48,27 @@ class Ex05Controller extends AbstractController
         }
         return $this->redirectToRoute('ex05_index');
     }
+
+	/**
+	 * @Route("/ex05/delete/{id}", name="ex05_delete", methods={"POST"})
+	 */
+	public function delete(EntityManagerInterface $em, $id)
+	{
+		$user = $em->getRepository(User::class)->find($id);
+		if ($user)
+		{
+			try
+			{
+				$userUsername = $user->getUsername();
+				$em->remove($user);
+				$em->flush();
+				$this->addFlash('success', 'Utilisateur ' . $userUsername . ' dont l\'id est ' . $id . ' a été supprimé avec succès.');
+			}
+			catch (\Exception $e)
+			{
+				$this->addFlash('error', 'Erreur lors de la suppression de l\'utilisateur : ' . $id . ' ' . $e->getMessage());
+			}
+		}
+		return $this->redirectToRoute('ex05_index');
+	}
 }
