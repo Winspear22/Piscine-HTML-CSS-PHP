54b2462094833b391b5c6f755238b438ca495183
Winspear22 <adnen.aloui2@hotmail.com>
2025-06-30 18:31:39 +0200

    PHP05: avancement exercice 03.

--- Day-05-restart/ex03-retry/src/Ex03Bundle/Controller/Ex03Controller.php
+++ Day-05-restart/ex03-retry/src/Ex03Bundle/Controller/Ex03Controller.php
@@ -2,17 +2,50 @@
 
 namespace App\Ex03Bundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class Ex03Controller extends AbstractController
 {
     /**
-     * @Route("/ex03bundle", name="ex03bundle_index")
+     * @Route("/ex03", name="ex03_index")
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        return new Response("Hello from Ex03Controller!");
+		$users = $em->getRepository(User::class)->findAll();
+		return $this->render('index.html.twig',
+			['users' => $users]);
     }
+
+	/**
+	 * @Route("/ex03/create", name="ex03_create")
+	 */
+	public function create(Request $request, EntityManagerInterface $em): Response
+	{
+			$user = new User();
+			$form = $this->createForm(UserType::class, $user);
+			$form->handleRequest($request);
+
+			if ($form->isSubmitted() && $form->isValid())
+			{
+				try 
+				{
+					$em->persist($user);
+					$em->flush();
+					$this->addFlash('success', 'Utilisateur créé avec succès.');
+					return $this->redirectToRoute('ex03_index');
+				}
+				catch (\Exception $e)
+				{
+					$this->addFlash('error', 'Erreur lors de la création de l\'utilisateur : ' . $e->getMessage());
+				}
+			}
+		return $this->render('create.html.twig',
+			['form' => $form->createView()]);
+	}
 }
