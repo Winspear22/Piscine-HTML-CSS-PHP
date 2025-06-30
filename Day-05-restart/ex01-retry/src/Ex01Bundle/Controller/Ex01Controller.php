8010ac886dda6b4095541d48d32c54e5200f4630
Winspear22 <adnen.aloui2@hotmail.com>
2025-06-30 14:20:02 +0200

    PHP05 -- ex01 : terminé.

--- Day-05-restart/ex01-retry/src/Ex01Bundle/Controller/Ex01Controller.php
+++ Day-05-restart/ex01-retry/src/Ex01Bundle/Controller/Ex01Controller.php
@@ -2,17 +2,43 @@
 
 namespace App\Ex01Bundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class Ex01Controller extends AbstractController
 {
     /**
      * @Route("/ex01", name="ex01_index")
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        return new Response("Hello from Ex01Controller!");
+        if ($request->isMethod('POST')) 
+        {
+            $schemaManager = $em->getConnection()->createSchemaManager();
+            $schemaTool = new SchemaTool($em);
+            $metadata = [$em->getClassMetadata(User::class)];
+            try 
+            {
+                if ($schemaManager->tablesExist(['user']))
+                    $this->addFlash('notice', "La table 'user' existe déjà.");
+                else 
+                {
+                    $schemaTool->createSchema($metadata);
+                    $this->addFlash('notice', "La table 'user' a été créée avec succès.");
+                }
+            } 
+            catch (\Exception $e) 
+            {
+                $this->addFlash('error', "Erreur lors de la création de la table : " . $e->getMessage());
+            }
+            return $this->redirectToRoute('ex01_index');
+        }
+
+        return $this->render('index.html.twig');
     }
 }
