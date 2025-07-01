6e7c36efcb65a91c2e8f1562f782e267fd23368f
Winspear22 <adnen.aloui2@hotmail.com>
2025-07-01 21:37:02 +0200

    PHP05 -- ex07 : TERMINE.

--- Day-05-restart/ex07-retry/config/bundles.php
+++ Day-05-restart/ex07-retry/config/bundles.php
@@ -3,4 +3,9 @@
 return [
     App\Ex07Bundle\Ex07Bundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
--- Day-05-restart/ex07-retry/src/Ex07Bundle/Controller/Ex07Controller.php
+++ Day-05-restart/ex07-retry/src/Ex07Bundle/Controller/Ex07Controller.php
@@ -2,17 +2,94 @@
 
 namespace App\Ex07Bundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class Ex07Controller extends AbstractController
 {
     /**
-     * @Route("/ex07bundle", name="ex07bundle_index")
+     * @Route("/ex07", name="ex07_index")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
-        return new Response("Hello from Ex07Controller!");
+        $users = $em->getRepository(User::class)->findAll();
+        return $this->render('index.html.twig', ['users' => $users]);
+    }
+
+    /**
+     * @Route("/ex07/create", name="ex07_create")
+     */
+    public function createRandomUsers(EntityManagerInterface $em)
+    {
+        $i = 0;
+		$checkUsersExistence = $em->getRepository(User::class)->findAll();
+		if (count($checkUsersExistence) > 0)
+		{
+			foreach ($checkUsersExistence as $users)
+				$em->remove($users);
+			$em->flush();
+		}
+        while ($i < 10)
+        {
+            $user = new User();
+            $user->setUsername('user' . $i);
+            $user->setName('Nom' . $i);
+            $user->setEmail('user' . $i . '@mail.com');
+            $user->setEnable(rand(0, 1) === 1);
+            $user->setBirthdate(new \DateTime('-' . rand(18, 40) . ' years'));
+            $user->setAddress('Adresse ' . $i . ' avenue Testville');
+            $em->persist($user);
+            $i++;
+        }
+        try 
+        {
+            $em->flush();
+            $this->addFlash('success', 'Utilisateur ' . $i . ' créé avec succès.');
+        } 
+        catch (\Exception $e) 
+        {
+            $this->addFlash('error', 'Erreur lors de la création de l\'utilisateur ' . $i . ': ' . $e->getMessage());
+        }
+        return $this->redirectToRoute('ex07_index');
+    }
+
+    /**
+     * @Route("/ex07/update/{id}", name="ex07_update")
+     */
+    public function updateUser(Request $request, EntityManagerInterface $em, int $id): Response
+    {
+        $user = $em->getRepository(User::class)->find($id);
+        if (!$user)
+		{
+            $this->addFlash('error', 'Utilisateur avec l\'id ' . $id . ' non trouvé.');
+            return $this->redirectToRoute('ex07_index');
+        }
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+		{
+            try
+			{
+                $em->flush();
+                $this->addFlash('success', 'Utilisateur modifié avec succès.');
+                return $this->redirectToRoute('ex07_index');
+            } 
+			catch (\Exception $e)
+			{
+                $this->addFlash('error', 'Erreur lors de la mise à jour de l\'utilisateur : ' . $e->getMessage());
+            }
+        }
+
+        return $this->render('update.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user
+        ]);
     }
 }
