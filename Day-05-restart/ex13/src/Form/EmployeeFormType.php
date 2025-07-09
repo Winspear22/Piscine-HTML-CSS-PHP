b20f0a5f32dde3e061038849da09348b8900bfd3
Winspear22 <adnen.aloui2@hotmail.com>
2025-07-09 18:08:35 +0200

    PHP05 -- ex13: formtype brut crée

---
+++ Day-05-restart/ex13/src/Form/EmployeeFormType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employee;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmployeeFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('email')
+            ->add('birthdate')
+            ->add('active')
+            ->add('employed_since')
+            ->add('employed_until')
+            ->add('hours')
+            ->add('salary')
+            ->add('position')
+            ->add('manager', EntityType::class, [
+                'class' => Employee::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Employee::class,
+        ]);
+    }
+}
