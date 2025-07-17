705d949f7d771b2484114df756a2415ce5ab6e0e
Winspear22 <adnen.aloui2@hotmail.com>
2025-07-17 17:36:26 +0200

    PHP06 -- ex01 : création du controller d'authentification.

--- Day-06/ex01/src/Entity/User.php
+++ Day-06/ex01/src/Entity/User.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -47,5 +49,35 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    #[ORM\Column(type: 'json')]
+    private array $roles = [];
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+    $this->roles = $roles;
+    return $this;
+    }
+
+    public function getUserIdentifier(): string
+    {
+    return $this->username ?? '';
+    }
+
+    // getUsername est gardé pour compatibilité ascendante (certains bundles utilisent encore ce nom)
+    public function eraseCredentials()
+    {
+    // If you store any temporary, sensitive data on the user, clear it here
+    // $this->plainPassword = null;
     }
 }
